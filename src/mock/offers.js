333482e7d346fc08070f3d9dcd960cca29976d9c
Камила <105364337+Camihak@users.noreply.github.com>
2025-04-21 11:28:09 +0300

    Adds mock data for offers

---
+++ src/mock/offers.js
@@ -0,0 +1,34 @@
+const mockOffers = [
+  {
+    type: 'taxi',
+    offers: [
+    {
+    id: 'b4c3e4e6-9053-42ce-b747-e281314baa31',
+    title: 'Upgrade to a business class',
+    price: 100
+    }
+    ]
+  },
+  {
+    type: 'taxi',
+    offers: [
+    {
+    id: 'b4c3e4e6-9053-42ce-b747-e281314baa32',
+    title: 'Upgrade to a business class',
+    price: 110
+    }
+    ]
+    },
+    {
+      type: 'taxi',
+      offers: [
+      {
+      id: 'b4c3e4e6-9053-42ce-b747-e281314baa33',
+      title: 'Upgrade to a business class',
+      price: 120
+      }
+      ]
+      }
+  ];
+
+export {mockOffers};
