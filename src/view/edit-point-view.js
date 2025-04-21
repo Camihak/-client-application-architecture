b80c1faee7bfa576ef1860b882e16139434e2215
Камила <105364337+Camihak@users.noreply.github.com>
2025-04-21 12:13:24 +0300

    Adds the ability to pass data to the component, adds data to the component

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -1,6 +1,36 @@
+import dayjs from 'dayjs';
 import { createElement } from '../render';
 
-function createEditPointTemplate() {
+const DATE_TIME_FORMAT = 'DD/MM/YY HH:mm';
+
+const getOffersMarking = (selectedOffers, unselectedOffers) => {
+  let offersList = '';
+  selectedOffers.forEach((selectedOffer) => {
+    offersList += `<div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
+    <label class="event__offer-label" for="event-offer-luggage-1">
+      <span class="event__offer-title">${selectedOffer.title}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${selectedOffer.price}</span>
+    </label>
+  </div>`
+  })
+  unselectedOffers.forEach((unselectedOffer) => {
+    offersList += `<div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-meal">
+    <label class="event__offer-label" for="event-offer-meal-1">
+      <span class="event__offer-title">${unselectedOffer.title}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${unselectedOffer.price}</span>
+    </label>
+  </div>`
+ })
+ return offersList;
+}
+
+function createEditPointTemplate(point, selectedOffers, unselectedOffers, destination) {
+  const {basePrice, type, dateFrom, dateTo} = point;
+  const {name, description} = destination;
   return(
     `<li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
@@ -66,9 +96,9 @@ function createEditPointTemplate() {
 
         <div class="event__field-group  event__field-group--destination">
           <label class="event__label  event__type-output" for="event-destination-1">
-            Flight
+            ${type}
           </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${name}" list="destination-list-1">
           <datalist id="destination-list-1">
             <option value="Amsterdam"></option>
             <option value="Geneva"></option>
@@ -78,10 +108,10 @@ function createEditPointTemplate() {
 
         <div class="event__field-group  event__field-group--time">
           <label class="visually-hidden" for="event-start-time-1">From</label>
-          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
+          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dayjs(dateFrom).format(DATE_TIME_FORMAT)}">
           &mdash;
           <label class="visually-hidden" for="event-end-time-1">To</label>
-          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
+          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dayjs(dateTo).format(DATE_TIME_FORMAT)}">
         </div>
 
         <div class="event__field-group  event__field-group--price">
@@ -89,7 +119,7 @@ function createEditPointTemplate() {
             <span class="visually-hidden">Price</span>
             &euro;
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
+          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
@@ -101,58 +131,14 @@ function createEditPointTemplate() {
       <section class="event__details">
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
           <div class="event__available-offers">
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-              <label class="event__offer-label" for="event-offer-luggage-1">
-                <span class="event__offer-title">Add luggage</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">50</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-              <label class="event__offer-label" for="event-offer-comfort-1">
-                <span class="event__offer-title">Switch to comfort</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">80</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-              <label class="event__offer-label" for="event-offer-meal-1">
-                <span class="event__offer-title">Add meal</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">15</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-              <label class="event__offer-label" for="event-offer-seats-1">
-                <span class="event__offer-title">Choose seats</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">5</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-              <label class="event__offer-label" for="event-offer-train-1">
-                <span class="event__offer-title">Travel by train</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">40</span>
-              </label>
-            </div>
+          ${getOffersMarking(selectedOffers, unselectedOffers)}
           </div>
         </section>
 
         <section class="event__section  event__section--destination">
           <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-          <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
+          <p class="event__destination-description">${description}</p>
         </section>
       </section>
     </form>
@@ -161,8 +147,26 @@ function createEditPointTemplate() {
 }
 
 export default class EditPointView {
+  constructor(point, offers, destinations) {
+    this.point = point;
+    this.offersElements = offers;
+    this.destinations = destinations;
+  }
+
   getTemplate() {
-    return createEditPointTemplate();
+    const destination = this.destinations.find((destination) => destination.id === this.point.destination);
+    let selectedOffers = [];
+    let unselectedOffers = [];
+    this.offersElements.forEach((offerElement) => {
+      offerElement.offers.forEach((offer) => {
+        if (this.point.offers.includes(offer.id)) {
+          selectedOffers.push(offer);
+        } else {
+          unselectedOffers.push(offer);
+        }
+      })
+    })
+    return createEditPointTemplate(this.point, selectedOffers, unselectedOffers, destination);
   }
 
   getElement() {
--- src/view/point-view.js
+++ src/view/point-view.js
@@ -1,34 +1,87 @@
+import dayjs from 'dayjs';
 import { createElement } from '../render';
 
-function createPointTemplate() {
+const DATE_FORMAT = 'D MMM';
+const TIME_FORMAT = 'HH:mm';
+
+function createOffersTemplate(offers) {
+  let offersList = '';
+  if (offers.length != 0) {
+    offersList += `<ul class="event__selected-offers">`;
+    for (let i = 0; i < offers.length; i++) {
+      offersList += `<li class="event__offer"><span class="event__offer-title">${offers[i].title}</span>&plus;&euro;&nbsp;<span class="event__offer-price">${offers[i].price}</span></li>`;
+    }
+    offersList += `</ul>`
+  }
+  return offersList;
+}
+
+function getTransportPhoto(pictures) {
+  let transportPhoto;
+  pictures.forEach((picture) => transportPhoto = picture.src);
+  return transportPhoto;
+}
+
+function getDispatchDate(dateFrom) {
+  return dateFrom ? dayjs(dateFrom).format(DATE_FORMAT): '';
+}
+
+function getDepartureTime(dateFrom){
+    return dayjs(dateFrom).format(TIME_FORMAT);
+}
+
+function getArrivalTime(dateFrom, dateTo) {
+  if (dayjs(dateFrom).format(DATE_FORMAT) === dayjs(dateTo).format(DATE_FORMAT)) {
+    return dayjs(dateTo).format(TIME_FORMAT);
+  }
+  return dayjs(dateTo).format(DATE_FORMAT + ' ' + TIME_FORMAT);
+}
+
+function getTravelTime(dateFrom, dateTo) {
+  let travelTime = '';
+  const daysNumber = dayjs(dateTo).diff(dayjs(dateFrom), 'd');
+  const hoursNumber = dayjs(dateTo).diff(dayjs(dateFrom), 'h') % 24;
+  const minutesNumber = dayjs(dateTo.slice(0,16)).diff(dayjs(dateFrom.slice(0,16)), 'm') % 60;
+  if (daysNumber === 1) {
+    travelTime += '1 DAY ';
+  } else if (daysNumber > 1) {
+    travelTime += `${daysNumber} DAYS `;
+  }
+  if (hoursNumber === 1) {
+    travelTime += '1 HOUR ';
+  } else if (hoursNumber > 1) {
+    travelTime += `${hoursNumber} HOURS `;
+  }
+  (minutesNumber > 0) ? travelTime += `${minutesNumber} MIN `:'';
+  return travelTime;
+}
+
+function createPointTemplate(point, offers, destanation) {
+  const {basePrice, isFavorite, type, dateFrom, dateTo} = point;
+
+  const {name, pictures} = destanation;
   return (
     `<li class="trip-events__item">
     <div class="event">
-      <time class="event__date" datetime="2019-03-18">MAR 18</time>
+      <time class="event__date" datetime="${dateFrom}">${getDispatchDate(dateFrom)}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="${getTransportPhoto(pictures)}" alt="Event type icon">
       </div>
-      <h3 class="event__title">Taxi Amsterdam</h3>
+      <h3 class="event__title">${type} ${name}</h3>
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          <time class="event__start-time" datetime="${dayjs(dateFrom).format('YYYY-MM-DD'+'T'+TIME_FORMAT)}">${getDepartureTime(dateFrom)}</time>
           &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          <time class="event__end-time" datetime="${dayjs(dateTo).format('YYYY-MM-DD'+'T'+TIME_FORMAT)}">${getArrivalTime(dateFrom, dateTo)}</time>
         </p>
-        <p class="event__duration">30M</p>
+        <p class="event__duration">${getTravelTime(dateFrom, dateTo)}</p>
       </div>
       <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">20</span>
+        &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
       </p>
       <h4 class="visually-hidden">Offers:</h4>
-      <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
-          &plus;&euro;&nbsp;
-          <span class="event__offer-price">20</span>
-        </li>
-      </ul>
-      <button class="event__favorite-btn event__favorite-btn--active" type="button">
+      ${createOffersTemplate(offers)}
+      <button class="event__favorite-btn ${(isFavorite) ? 'event__favorite-btn--active': ''}" type="button">
         <span class="visually-hidden">Add to favorite</span>
         <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
           <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -43,8 +96,24 @@ function createPointTemplate() {
 }
 
 export default class PointView {
+
+  constructor(point, offers, destinations) {
+    this.point = point;
+    this.offersElements = offers;
+    this.destinations = destinations;
+  }
+
   getTemplate() {
-    return createPointTemplate();
+    const destination = this.destinations.find((destination) => destination.id === this.point.destination);
+    let currentOffers = [];
+    this.offersElements.forEach((offerElement) => {
+      offerElement.offers.forEach((offer) => {
+        if (this.point.offers.includes(offer.id)) {
+          currentOffers.push(offer);
+        }
+      })
+    })
+    return createPointTemplate(this.point, currentOffers, destination);
   }
 
   getElement() {
