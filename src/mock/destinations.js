cfbce86edfdfab80bd2eb215fca611bbc6be7ea8
Камила <105364337+Camihak@users.noreply.github.com>
2025-04-21 11:30:15 +0300

    adds mock data for destinations

---
+++ src/mock/destinations.js
@@ -0,0 +1,37 @@
+const mockDestinations = [
+  {
+    id: 'cfe416cq-10xa-ye10-8077-2fs9a01edcab',
+    description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Voronezh',
+    pictures: [
+    {
+    src: 'https://storage.yandexcloud.net/moskvichmag/uploads/2024/03/Xcite-X-Cross7-768x403.jpg',
+    description: 'Chamonix parliament building'
+    }
+    ]
+  },
+  {
+    id: 'cfe316cq-10xa-ye10-8077-2fs9a01edcab',
+    description: 'Moscow, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Moscow',
+    pictures: [
+    {
+    src: 'https://loremflickr.com/248/152?random=87',
+    description: 'Moscow parliament building'
+    }
+    ]
+  },
+  {
+    id: 'cfe216cq-10xa-ye10-8077-2fs9a01edcab',
+    description: 'New York, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'New York',
+    pictures: [
+    {
+    src: 'https://loremflickr.com/248/152?random=34',
+    description: 'New York parliament building'
+    }
+    ]
+  }
+  ]
+
+  export {mockDestinations};
