4cf284cc0f6594ea3cd642d925a8f1e6b64c162b
Камила <105364337+Camihak@users.noreply.github.com>
2025-04-21 11:27:36 +0300

    Add mock data for points

---
+++ src/mock/points.js
@@ -0,0 +1,40 @@
+const mockPoints = [
+  {
+  id: 'f4b62099-293f-4c3d-a702-94eec4a2808c',
+  basePrice: 1100,
+  dateFrom: '2019-07-09T08:20:56',
+  dateTo: '2019-07-09T09:22:13',
+  destination: 'cfe416cq-10xa-ye10-8077-2fs9a01edcab',
+  isFavorite: false,
+  offers: [
+  'b4c3e4e6-9053-42ce-b747-e281314baa31', 'b4c3e4e6-9053-42ce-b747-e281314baa32'
+  ],
+  type: 'taxi'
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2807c',
+    basePrice: 1000,
+    dateFrom: '2019-08-06T22:55:56',
+    dateTo: '2019-08-08T12:22:13.375Z',
+    destination: 'cfe316cq-10xa-ye10-8077-2fs9a01edcab',
+    isFavorite: true,
+    offers: [
+    'b4c3e4e6-9053-42ce-b747-e281314baa33'
+    ],
+    type: 'bus'
+    },
+    {
+      id: 'f4b62099-293f-4c3d-a702-94eec4a2806c',
+      basePrice: 900,
+      dateFrom: '2019-01-10T11:55:56',
+      dateTo: '2019-01-10T16:22:13.375Z',
+      destination: 'cfe216cq-10xa-ye10-8077-2fs9a01edcab',
+      isFavorite: false,
+      offers: [
+      'b4c3e4e6-9053-42ce-b747-e281314baa33'
+      ],
+      type: 'flight'
+      }
+  ];
+
+  export {mockPoints};
